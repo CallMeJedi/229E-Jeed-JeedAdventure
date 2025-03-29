cf9a9e2b74d5621e960ac15d81f5183fb6fd32f6
CallMeJedi <110235599+CallMeJedi@users.noreply.github.com>
2025-03-29 17:07:29 +0700

    All Map Decorations Added
    -Muse

---
+++ Assets/Scripts/ThrowRock.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ThrowRock : MonoBehaviour
+{
+    [SerializeField] float force, mass, acc;
+
+    public void CalculateForce()
+    {
+        force = mass * acc;
+        GetComponent<Rigidbody>().AddRelativeForce(0, force,0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CalculateForce();
+        }
+    }
+}
