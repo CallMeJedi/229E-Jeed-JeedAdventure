7c0b55ec1e29b3de7f5da464199bd450310cc424
CallMeJedi <110235599+CallMeJedi@users.noreply.github.com>
2025-03-29 20:54:15 +0700

    Timer Added
    -Muse

---
+++ Assets/Scripts/Stopwatch.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class Stopwatch : MonoBehaviour
+{
+    [SerializeField] TMP_Text timerText;
+    [SerializeField] float time;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timerText.text = time.ToString();
+    }
+}
