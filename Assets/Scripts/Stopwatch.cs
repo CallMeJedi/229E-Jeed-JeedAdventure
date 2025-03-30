0d1c41184ac7e2bac4450a3a247ac7ad47f63490
CallMeJedi <110235599+CallMeJedi@users.noreply.github.com>
2025-03-30 14:07:11 +0700

    Adjust Things 2

--- Assets/Scripts/Stopwatch.cs
+++ Assets/Scripts/Stopwatch.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 public class Stopwatch : MonoBehaviour
 {
     [SerializeField] TMP_Text timerText;
-    [SerializeField] float time;
+    [SerializeField] float time = 90f;
     
     void Update()
     {
-        time += Time.deltaTime;
+        time -= Time.deltaTime;
         timerText.text = time.ToString();
-
-        if (time >= 90 ) //Can't win within 90 sec will restart
+        
+        if (time <= 0 ) //if time out will restart
         {
             SceneManager.LoadScene("SampleScene");
         }
